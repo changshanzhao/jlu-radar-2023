5356091756af5d42664dc167cf47f8428a89e187
junbo.zhao <2395624242@qq.com>
2023-05-22 12:32:19 +0000

    update yolov5_ros/src/detect.py. 添加中心点
    Signed-off-by: junbo.zhao <2395624242@qq.com>

--- yolov5_ros/src/detect.py
+++ yolov5_ros/src/detect.py
@@ -199,7 +199,7 @@ class Yolov5Detector:
                     cv2.putText(im0, f"{torch.sigmoid(d[8]).item():.2f}", pt3, 1, 1, (0, 255, 0))
                     cv2.putText(im0, self.names[int(d[-1])], (pt1[0],pt1[1]+15), 1, 1, (0, 255, 0))
                     cv2.putText(im0, f"{torch.sigmoid(d[8]).item():.2f}", pt2, 1, 1, (0, 255, 0))
-
+                    p_ave = (int((int(d[0])+int(d[2])+int(d[4])+int(d[6]))/4), int((int(d[1])+int(d[3])+int(d[5])+int(d[7]))/4))
 
 
         # det = pred[0].cpu().numpy()
